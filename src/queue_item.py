db3b330e2bd12797c21b8f4bb5aedaaa0f195908
FerranGuardia <onlyartferran@gmail.com>
2025-12-25 22:48:31 +0100

    refactor: fix initialization method and add validation

--- src/queue_item.py
+++ src/queue_item.py
@@ -1,6 +1,21 @@
+import os #this is a python module that will allow me to interact with the os.
+
 class QueueItem:
-    def _inint__(self, file_path, output_folder):
+
+    def _init__(self, file_path, output_folder):
+
+        #---Validation ---
+
+        if not os.path.isfile(file_path):
+            raise FileNotFoundError(f"Input file does not exist: {file_path}") #this checks if the input file exist. If it doesn't raises an error
+        
+        if not os.path.isdir(output_folder):
+            raise NotADirectoryError(f"Output folder does not exist: {output_folder}") #this checks if the output folder exist. If it doesn't raises an error
+
+
         self.file_path = file_path  #this is the location of the file
+        self.file_name = os.path.basename(file_path) #this line extracts a full path and figures out the file name
         self.output_folder = output_folder #this is where the file will be placed
         self.status = "Pending" #this is the default status when you add an item to the queue
         self.progress = 0 #this is to show the progress until completion
+        
