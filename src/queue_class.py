e11777c9f531bdd7c05cde0d4a0736ed1d7e7122
FerranGuardia <onlyartferran@gmail.com>
2025-12-27 16:27:27 +0100

    chore: add methods comment and improve spacing for redibility

--- src/queue_class.py
+++ src/queue_class.py
@@ -8,6 +8,8 @@ class Queue:
     self.items = []
     self.current_index = None
 
+#---Methods---
+
 def add_item(self, input_folder: str, output_folder: str):
     item = QueueItem(input_folder, output_folder)
     self.items.append(item)
