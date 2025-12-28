96836a5339bbc587cbe602958d0626b325538343
FerranGuardia <onlyartferran@gmail.com>
2025-12-28 13:52:34 +0100

    feat: add remove item() method

--- src/queue_class.py
+++ src/queue_class.py
@@ -9,6 +9,7 @@ class Queue:
     
     @property
     def current_item(self):
+
         if self.current_index is None:
             return None
         return self.items[self.current_index]
@@ -16,6 +17,22 @@ class Queue:
     #---Methods---
 
     def add_item(self, input_folder: str, output_folder: str):
+
         item = QueueItem(input_folder, output_folder)
         self.items.append(item)
         return item
+    
+    def remove_item(self, index: int):
+        # Validate index
+        if index < 0 or index >= len(self.items):
+            raise IndexError("Index {index} is out of range")
+
+        if self.current_index is not None:
+          if index == self.current_index:
+              self.current_index = None
+          elif index < self.curent_index:
+              self.current_index -= 1
+        return self.items.pop(index)
+      
+      
+      
