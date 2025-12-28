c25201071983f119591d3d2fcad78a97e147fc61
FerranGuardia <onlyartferran@gmail.com>
2025-12-28 14:10:25 +0100

    fix: correct typo errors and ajust indentation for concistency

--- src/queue_class.py
+++ src/queue_class.py
@@ -2,37 +2,32 @@ from src.queue_item import QueueItem
 
 #---Manager---
 class Queue:
-    
+
     def __init__(self):
         self.items = []
         self.current_index = None
     
     @property
     def current_item(self):
-
         if self.current_index is None:
             return None
         return self.items[self.current_index]
-  
+
     #---Methods---
 
     def add_item(self, input_folder: str, output_folder: str):
-
         item = QueueItem(input_folder, output_folder)
         self.items.append(item)
         return item
     
     def remove_item(self, index: int):
-        # Validate index
         if index < 0 or index >= len(self.items):
-            raise IndexError("Index {index} is out of range")
-
+            raise IndexError(f"Index {index} is out of range")
+        
         if self.current_index is not None:
-          if index == self.current_index:
-              self.current_index = None
-          elif index < self.curent_index:
-              self.current_index -= 1
+            if index == self.current_index:
+                self.current_index = None
+            elif index < self.current_index:
+                self.current_index -= 1
+        
         return self.items.pop(index)
-      
-      
-      
