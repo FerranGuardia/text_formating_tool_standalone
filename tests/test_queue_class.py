87c7ebd5469b58d4b2e856ec5ca844bd264d62bf
FerranGuardia <onlyartferran@gmail.com>
2025-12-28 22:46:58 +0100

    test: add pytest.fixture queue 3 items
    -apply fixture to test_order_items_added
    -create behavioral test of a scenario for remove item

--- tests/test_queue_class.py
+++ tests/test_queue_class.py
@@ -3,6 +3,15 @@ from src.queue_class import Queue
 from src.queue_item import QueueItem
 
 @pytest.fixture
+def queue_with_three_items(folders):
+    input_folder, output_folder = folders
+    queue = Queue()
+    item1 = queue.add_item(input_folder, output_folder)
+    item2 = queue.add_item(input_folder, output_folder)
+    item3 = queue.add_item(input_folder, output_folder)
+    return queue, item1, item2, item3
+
+@pytest.fixture
 def folders(tmp_path):
 
     input_folder = tmp_path / "book"
@@ -39,13 +48,9 @@ def test_is_current_item_in_queue_none():
     queue = Queue()
     assert queue.current_item is None
 
-def test_order_items_added(folders):
+def test_order_items_added(queue_with_three_items):
 
-    input_folder, output_folder = folders
-    queue = Queue()
-    item1 = queue.add_item(input_folder, output_folder)
-    item2 = queue.add_item(input_folder, output_folder)
-    item3 = queue.add_item(input_folder, output_folder)
+    queue, item1, item2, item3 = queue_with_three_items
 
     assert len(queue.items) == 3
     assert queue.items[0] is item1
@@ -77,4 +82,14 @@ def test_remove_item_empty_queue():
     with pytest.raises(IndexError):
         queue.remove_item(0)
 
+# Current Index Scenario Test
+def test_remove_current_item_sets_current_index_to_none(queue_with_three_items):
 
+    queue, item1, item2, item3 = queue_with_three_items
+
+    queue.current_index = 1  
+    removed = queue.remove_item(1)
+
+    assert removed is item2
+    assert queue.current_index is None  # Should be decremented from 2 to 1
+    assert queue.items[1] is item3
