4a4279cb56352cfca0e7ac01c711f4277308ebc1
FerranGuardia <onlyartferran@gmail.com>
2025-12-28 02:11:11 +0100

    feat: Add property current_item()

--- src/queue_class.py
+++ src/queue_class.py
@@ -1,16 +1,21 @@
 from src.queue_item import QueueItem
 
-
 #---Manager---
 class Queue:
     
  def __init__(self):
     self.items = []
     self.current_index = None
-
+    
+ @property
+ def current_item(self):
+    if self.current_index is None:
+        return None
+    return self.items[self.current_index]
+  
 #---Methods---
 
- def add_item(self, input_folder: str, output_folder: str):
+  def add_item(self, input_folder: str, output_folder: str):
     item = QueueItem(input_folder, output_folder)
     self.items.append(item)
     return item
